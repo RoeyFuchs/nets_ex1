0e4dcd2ec27d01ce759d97d26267d108f876b22d
RoeyFuchs <fuchsroey@gmail.com>
2020-11-09 12:12:15 +0200

    add client doc

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 import socket
 import sys
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # create IP4 UDP socket
 while True:
-    msg = input().encode()
-    s.sendto(msg, (sys.argv[1], int(sys.argv[2])))
-    data, addr = s.recvfrom(1024)
-    print(data.decode().split(',')[1])
+    msg = input().encode() # get domain from user
+    s.sendto(msg, (sys.argv[1], int(sys.argv[2]))) # send the domain to server (got as arguments)
+    data, addr = s.recvfrom(1024) # recive answer
+    print(data.decode().split(',')[1]) # print IP
 s.close()
