10482cd5c98cf9916e9df28cda291f2861ca4f39
RoeyFuchs <fuchsroey@gmail.com>
2020-11-15 18:26:37 +0200

    empty lines bug

--- server.py
+++ server.py
@@ -1,52 +1,57 @@
+import socket
 import sys
 from datetime import datetime
-import socket
 
 TIME_FORMAT = "%d-%m-%Y-%H-%M-%S"
+
+
 def get_date_as_str(date):
     """ this function return date string from datetime object """
     return date.strftime(TIME_FORMAT)
+
 
 def get_date_from_str(date_str):
     """ this function return datetime object from string """
     return datetime.strptime(date_str, TIME_FORMAT)
 
+
 def parse_entery(ent):
     """ this function parse entery to known format """
-    separator = ','
+    separator = ","
     ents = ent.split(separator)
-    if len(ents) < 4: # in a case of static entery
+    if len(ents) < 4:  # in a case of static entery
         ents.append(datetime.max)
-    else: # dynemic entery
+    else:  # dynemic entery
         ents[3] = get_date_from_str(ents[3])
     return ents
 
+
 def write_to_file(file_path, enterys_dict):
     """ write enterys dict to file """
-    with open(file_path, 'w') as f:
+    with open(file_path, "w") as f:
         for i in enterys_dict:
             entery = [i, enterys_dict[i]["ip"], enterys_dict[i]["ttl"]]
-            # check if the entery is dymanic 
+            # check if the entery is dymanic
             if enterys_dict[i]["last_modf"] is not datetime.max:
                 entery.append(get_date_as_str(enterys_dict[i]["last_modf"]))
             f.write(",".join(entery) + "\n")
 
+
 def read_init_file(path):
-    """ this function read a file that contain entery in every line:
+    """this function read a file that contain entery in every line:
     name, ip, TTL
-    and return dictionary (key = name, value = dictionary of IP and TTL) """
+    and return dictionary (key = name, value = dictionary of IP and TTL)"""
     with open(path) as f:
         content = f.read().splitlines()
     enterys = {}
     for line in content:
+        if line == "":
+            continue
         name, ip, ttl, last_modf = parse_entery(line)
-        entery = {
-                "ip" : ip,
-                "ttl" : ttl,
-                "last_modf" : last_modf
-                }
+        entery = {"ip": ip, "ttl": ttl, "last_modf": last_modf}
         enterys[name] = entery
     return enterys
+
 
 def time_to_update(entery):
     """ this function check if we need to update the entery, return boolean """
@@ -61,12 +66,12 @@ names = read_init_file(sys.argv[4])
 
 ## open a sockets for clients and parent
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-client_socket.bind(('', int(sys.argv[1]))) # bind to requsted port
+client_socket.bind(("", int(sys.argv[1])))  # bind to requsted port
 # creat diffrent socket to connect parent for avoiding cases of
 # wating for parent answaer while new requsting from client
 parent_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-## get msg from client and handle 
+## get msg from client and handle
 while True:
     data, addr = client_socket.recvfrom(1024)
     data = data.decode()
@@ -76,14 +81,18 @@ while True:
         parent_socket.sendto(data.encode(), (parent_ip, parent_port))
         new_data, _ = parent_socket.recvfrom(1024)
         new_data = new_data.decode()
-        new_name, new_ip, new_ttl = new_data.split(',')
-        new_entery = {"ip" : new_ip, "ttl" : new_ttl, "last_modf" :
-                datetime.now()} # create the new entery
-        if data in names: del names[data] # in a case of update
-        names[new_name] = new_entery # save the new entery to dictionary
+        new_name, new_ip, new_ttl = new_data.split(",")
+        new_entery = {
+            "ip": new_ip,
+            "ttl": new_ttl,
+            "last_modf": datetime.now(),
+        }  # create the new entery
+        if data in names:
+            del names[data]  # in a case of update
+        names[new_name] = new_entery  # save the new entery to dictionary
         write_to_file(sys.argv[4], names)
     ans = ",".join([data, names[data]["ip"], names[data]["ttl"]])
-    client_socket.sendto(ans.encode(), addr) # send answer to client
+    client_socket.sendto(ans.encode(), addr)  # send answer to client
 
 ## close connection
 client_socket.close()
